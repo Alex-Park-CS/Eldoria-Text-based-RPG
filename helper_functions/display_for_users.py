5c9c8493613601dd521bab6e60fb2d713bf7902e
Alex-Park-CS <alexpark0930@gmail.com>
2023-12-06 12:44:36 -0800

    add doctests for print_character_stats, slow_print and print_board

--- helper_functions/display_for_users.py
+++ helper_functions/display_for_users.py
@@ -10,6 +10,15 @@ def print_board(board, character):
     :precondition board: a dictionary that represents the game board
     :precondition character: a dictionary that represents the character
     :postcondition: print the minimap of the game board
+
+    >>> board = {(0, 0): 'X', (0, 1): ' ', (0, 2): 'X',
+                 (1, 0): 'X', (1, 1): 'X', (1, 2): 'X',
+                 (2, 0): 'X', (2, 1): ' ', (2, 2): 'X'}
+    >>> character = {'x-position': 1, 'y-position': 1}
+    >>> print_board(board, character)
+    X X
+    XOX
+    X X
     """
 
     for col in range(21):
@@ -51,6 +60,21 @@ def intro_prompt():
 
 
 def slow_print(text, delay=0.007):
+    """
+    Print one character at a time with a delay between each character.
+    
+    :param text: a string
+    :param delay: a float
+    :precondition text: a string
+    :precondition delay: a float
+    :postcondition: print one character at a time with a delay between each character
+    
+    >>> slow_print("Hello, World!")
+    Hello, World!
+    
+    >>> slow_print("Hello, World!", 0.1)
+    Hello, World!
+    """
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
@@ -58,6 +82,22 @@ def slow_print(text, delay=0.007):
 
 
 def print_character_stats(character):
+    """
+    Print the character's stats.
+    
+    :param character: a dictionary
+    :precondition character: a dictionary that represents the character
+    :postcondition: print the character's stats
+    
+    >>> character = {'level': 1, 'maxHP': 10, 'currentHP': 10, 'atk': 1, 'gold': 0, 'currentEXP': 0, 'maxEXP': 10}
+    >>> print_character_stats(character)
+    ------Current Stats------
+    Your Level: 1
+    Your HP: 10 / 10
+    Your ATK: 1
+    Your gold: 0
+    Your EXP: 0 / 10 XP.
+    """
     slow_print(f"\n------Current Stats------\n"
                f"Your Level: {character['level']}\n"
                f"Your HP: {character['currentHP']} / {character['maxHP']}\n"
