7b896f996dfd4c0af52031606a0398f982e54537
Alex Park <alexpark0930@gmail.com>
2023-12-06 18:26:56 -0800

    remove warnings in character_module

--- helper_functions/character_module.py
+++ helper_functions/character_module.py
@@ -10,15 +10,15 @@ def is_alive(character):
     :postcondition: determines if the character's HP > 0
     :return: a boolean value
 
-    >>> character = {"name": "Ysera", "maxHP": 100, "currentHP": 100, "gold": 100, "level": 1,
+    >>> character_test = {"name": "Ysera", "maxHP": 100, "currentHP": 100, "gold": 100, "level": 1,
     ...              "maxEXP": 50, "currentEXP": 0, "atk": 5, "move": "Magic Missile",
     ...              "x-position": 10, "y-position": 20, "orb": 0}
     
-    >>> is_alive(character)
+    >>> is_alive(character_test)
     True
 
-    >>> character["currentHP"] = 0
-    >>> is_alive(character)
+    >>> character_test["currentHP"] = 0
+    >>> is_alive(character_test)
     False
 
     """
