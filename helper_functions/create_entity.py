1bee6c08d6976449a6492b89aadb380d66430fc1
Alex Park <alexpark0930@gmail.com>
2023-12-09 19:55:13 -0800

    fix doctest errors

--- helper_functions/create_entity.py
+++ helper_functions/create_entity.py
@@ -48,11 +48,10 @@ def make_character() -> dict:
     :return: a dictionary with the character stats
     
     >>> make_character()
-    {'name': 'Ysera', 'maxHP': 100, 'currentHP': 100, 'gold': 100, 'level': 1, 'maxEXP': 50, 
-    'currentEXP': 0, 'atk': 5, 'move': 'Magic Missile', 'x-position': 10, 'y-position': 20, 'orb': 0}
+    {'name': 'Ysera', 'maxHP': 100, 'currentHP': 100, 'gold': 0, 'level': 1, 'maxEXP': 50, 'currentEXP': 0, 'atk': 5, 'move': 'Magic Missile', 'x-position': 10, 'y-position': 20, 'orb': 0}
 
     """
-    character = {"name": "Ysera", "maxHP": 100, "currentHP": 100, "gold": 100, "level": 1,
+    character = {"name": "Ysera", "maxHP": 100, "currentHP": 100, "gold": 0, "level": 1,
                  "maxEXP": 50, "currentEXP": 0, "atk": 5, "move": "Magic Missile",
                  "x-position": 10, "y-position": 20, "orb": 0}
     return character
@@ -67,9 +66,7 @@ def make_pre_lv_10_field_mobs() -> list:
     :return: a list of dictionaries of pre-level 10 field mobs
     
     >>> make_pre_lv_10_field_mobs()
-    [{'name': 'Mutant Boar', 'currentHP': 50, 'maxHP': 50, 'atk': 5, 'move': 'Headbutt', 'EXP': 10, 'gold': 5,
-    'runnable': True}, {'name': 'Legion Soldier', 'currentHP': 100, 'maxHP': 100, 'atk': 10, 'move': 'Spear Thrust',
-    'EXP': 25, 'gold': 20, 'runnable': True}]
+    [{'name': 'Mutant Boar', 'currentHP': 50, 'maxHP': 50, 'move': 'Headbutt', 'atk': 5, 'EXP': 10, 'gold': 5, 'runnable': True}, {'name': 'Legion Soldier', 'currentHP': 100, 'maxHP': 100, 'move': 'Spear Thrust', 'atk': 10, 'EXP': 25, 'gold': 20, 'runnable': True}]
     """
     soldier = {"name": "Legion Soldier", "currentHP": 100, "maxHP": 100, "move": "Spear Thrust",
                "atk": 10, "EXP": 25, "gold": 20, "runnable": True}
@@ -88,14 +85,12 @@ def make_post_lv_10_field_mobs() -> list:
     :return: a list of dictionaries of post-level 10 field mobs
     
     >>> make_post_lv_10_field_mobs()
-    [{'name': 'Imp', 'currentHP': 100, 'maxHP': 100, 'atk': 15, 'EXP': 25, 'move': 'Tail Whip', 'gold': 20,
-    'runnable': True}, {'name': 'Legion Commander', 'currentHP': 400, 'maxHP': 400, 'move': 'Sword Slash',
-    'atk': 20, 'EXP': 60, 'gold': 75, 'runnable': True}]
+    [{'name': 'Imp', 'currentHP': 300, 'maxHP': 300, 'atk': 20, 'EXP': 35, 'move': 'Tail Whip', 'gold': 20, 'runnable': True}, {'name': 'Legion Commander', 'currentHP': 400, 'maxHP': 400, 'move': 'Sword Slash', 'atk': 40, 'EXP': 60, 'gold': 75, 'runnable': True}]
     """
-    imp = {"name": "Imp", "currentHP": 300, "maxHP": 300, "atk": 20, "EXP": 35,
-           "move": "Tail Whip", "gold": 20, "runnable": True}
-    commander = {"name": "Legion Commander", "currentHP": 400, "maxHP": 400,
-                 "move": "Sword Slash", "atk": 40, "EXP": 60, "gold": 75, "runnable": True}
+    imp = {'name': 'Imp', 'currentHP': 300, 'maxHP': 300, 'atk': 20, 'EXP': 35,
+           'move': 'Tail Whip', 'gold': 20, 'runnable': True}
+    commander = {'name': 'Legion Commander', 'currentHP': 400, 'maxHP': 400,
+                 'move': 'Sword Slash', 'atk': 40, 'EXP': 60, 'gold': 75, 'runnable': True}
     list_of_post_lvl_10_mobs = [imp, commander]
     return list_of_post_lvl_10_mobs
 
@@ -109,8 +104,7 @@ def make_marbas() -> dict:
     :return: a dictionary of the boss Marbas
     
     >>> make_marbas()
-    {'name': 'Marbas', 'currentHP': 800, 'maxHP': 800, 'atk': 40, 'EXP': 1000, 'gold': 300, 'move': 'Tainting Hands',
-    'runnable': False}
+    {'name': 'Marbas', 'currentHP': 800, 'maxHP': 800, 'atk': 65, 'EXP': 1000, 'gold': 300, 'move': 'Tainting Hands', 'runnable': False}
     """
     boss = {"name": "Marbas", "currentHP": 800, "maxHP": 800, "atk": 65, "EXP": 1000, "gold": 300,
             "move": "Tainting Hands", "runnable": False}
@@ -126,8 +120,7 @@ def make_andromalius() -> dict:
     :return: a dictionary of the boss Andromalius
     
     >>> make_andromalius()
-    {'name': 'Andromalius', 'currentHP': 900, 'maxHP': 900, 'atk': 35, 'EXP': 1100, 'gold': 350,
-    'move': "Serpent's Fang", 'runnable': False}
+    {'name': 'Andromalius', 'currentHP': 1000, 'maxHP': 1000, 'atk': 55, 'EXP': 1100, 'gold': 350, 'move': "Serpent's Fang", 'runnable': False}
     """
     boss = {"name": "Andromalius", "currentHP": 1000, "maxHP": 1000, "atk": 55, "EXP": 1100, "gold": 350,
             "move": "Serpent's Fang", "runnable": False}
@@ -143,8 +136,7 @@ def make_baal() -> dict:
     :return: a dictionary of the boss Baal
     
     >>> make_baal()
-    {'name': 'Baal', 'currentHP': 2000, 'maxHP': 2000, 'atk': 50, 'EXP': 2000, 'gold': 350,
-    'move': 'Thunderstorm', 'runnable': False}
+    {'name': 'Baal', 'currentHP': 2000, 'maxHP': 2000, 'atk': 70, 'EXP': 2000, 'gold': 400, 'move': 'Thunderstorm', 'runnable': False}
     """
     boss = {"name": "Baal", "currentHP": 2000, "maxHP": 2000, "atk": 70, "EXP": 2000, "gold": 400,
             "move": "Thunderstorm", "runnable": False}
@@ -160,11 +152,7 @@ def make_shop() -> list:
     :return: a list of dictionaries of items in the shop
     
     >>> make_shop()
-    [{'name': 'Pill of Vitality', 'price': 50, 'added_HP': 30, 'amount': 1,
-    'effect': 'Increases maxHP by 30 points'}, {'name': 'Elixer of Vigor', 'price': 65, 'added_ATK': 10,
-    'move_upgrade': 'Arcane Barrage', 'amount': 1, 'effect': 'Increases ATK by 10 points'},
-    {'name': 'Forbidden White Powder', 'price': 40, 'added_EXP': 100, 'amount': 2,
-    'effect': 'Increases currentEXP by 100 points'}]
+    [{'name': 'Pill of Vitality', 'price': 50, 'added_HP': 10, 'amount': 1, 'effect': 'Increases maxHP by 10 points'}, {'name': 'Elixir of Vigor', 'price': 65, 'added_ATK': 3, 'move_upgrade': 'Arcane Barrage', 'amount': 1, 'effect': 'Increases ATK by 3 points'}, {'name': 'Forbidden White Powder', 'price': 100, 'added_EXP': 100, 'amount': 50, 'effect': 'Increases currentEXP by 100 points'}]
     """
     pill_of_vitality = {"name": "Pill of Vitality", "price": 50, "added_HP": 10,
                         "amount": 1, "effect": "Increases maxHP by 10 points"}
